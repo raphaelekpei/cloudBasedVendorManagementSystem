65dd3dd91e2a53da3506fe1c8d299412f9ecd779
Raphael Ekpei <ekpeiraphael020@gmail.com>
2023-07-18 21:14:37 +0100

    Commit 100025

--- src/main/java/com/raphael/cloudbasedvendormanagementsystem/dto/request/UpdateCloudVendorRequest.java
+++ src/main/java/com/raphael/cloudbasedvendormanagementsystem/dto/request/UpdateCloudVendorRequest.java
@@ -1,12 +1,10 @@
-package com.raphael.cloudbasedvendormanagementsystem.dtos.request;
+package com.raphael.cloudbasedvendormanagementsystem.dto.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @AllArgsConstructor
-@NoArgsConstructor
 @Getter
 @Setter
 public class UpdateCloudVendorRequest {
