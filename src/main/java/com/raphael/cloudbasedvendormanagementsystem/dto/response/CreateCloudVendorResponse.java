3c9a1828047dff37046d99ec9f11175e6d79bafb
Raphael Ekpei <ekpeiraphael020@gmail.com>
2023-07-18 21:11:46 +0100

    Commit 100016

---
+++ src/main/java/com/raphael/cloudbasedvendormanagementsystem/dto/response/CreateCloudVendorResponse.java
@@ -0,0 +1,10 @@
+package com.raphael.cloudbasedvendormanagementsystem.dto.response;
+
+import lombok.*;
+
+@Getter
+@Builder
+public class CreateCloudVendorResponse {
+    private String message;
+
+}
