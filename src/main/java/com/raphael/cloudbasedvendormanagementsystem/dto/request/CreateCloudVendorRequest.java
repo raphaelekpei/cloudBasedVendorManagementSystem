1edba259def729579c73f6037e480250183aca55
Raphael Ekpei <ekpeiraphael020@gmail.com>
2023-07-18 21:11:28 +0100

    Commit 100015

--- src/main/java/com/raphael/cloudbasedvendormanagementsystem/dto/request/CreateCloudVendorRequest.java
+++ src/main/java/com/raphael/cloudbasedvendormanagementsystem/dto/request/CreateCloudVendorRequest.java
@@ -1,16 +1,15 @@
-package com.raphael.cloudbasedvendormanagementsystem.dtos.request;
+package com.raphael.cloudbasedvendormanagementsystem.dto.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.validation.annotation.Validated;
 
 @Getter
 @Setter
 @AllArgsConstructor
-@NoArgsConstructor
+
 public class CreateCloudVendorRequest {
+
     private String name;
     private String address;
     private String phoneNumber;
