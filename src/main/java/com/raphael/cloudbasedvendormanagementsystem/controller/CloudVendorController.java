8580c30a844dc19dff028d2f6ec8a4771fbf69b0
Raphael Ekpei <ekpeiraphael020@gmail.com>
2023-07-18 21:07:37 +0100

    Commit 10007

--- src/main/java/com/raphael/cloudbasedvendormanagementsystem/controller/CloudVendorController.java
+++ src/main/java/com/raphael/cloudbasedvendormanagementsystem/controller/CloudVendorController.java
@@ -1,58 +1,54 @@
 package com.raphael.cloudbasedvendormanagementsystem.controller;
 
 import com.raphael.cloudbasedvendormanagementsystem.data.model.CloudVendor;
-import com.raphael.cloudbasedvendormanagementsystem.dtos.request.CreateCloudVendorRequest;
-import com.raphael.cloudbasedvendormanagementsystem.dtos.request.UpdateCloudVendorRequest;
-import com.raphael.cloudbasedvendormanagementsystem.dtos.response.CreateCloudVendorResponse;
-import com.raphael.cloudbasedvendormanagementsystem.dtos.response.UpdateCloudVendorResponse;
+import com.raphael.cloudbasedvendormanagementsystem.dto.request.CreateCloudVendorRequest;
+import com.raphael.cloudbasedvendormanagementsystem.dto.request.UpdateCloudVendorRequest;
+import com.raphael.cloudbasedvendormanagementsystem.dto.response.CreateCloudVendorResponse;
+import com.raphael.cloudbasedvendormanagementsystem.dto.response.GetCloudVendorResponse;
+import com.raphael.cloudbasedvendormanagementsystem.dto.response.UpdateCloudVendorResponse;
 import com.raphael.cloudbasedvendormanagementsystem.service.CloudVendorService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("cloud_vendors")
-@RequiredArgsConstructor
+@RequestMapping("api/v1/cloud-vendors")
+@AllArgsConstructor
 public class CloudVendorController {
-
-    @Autowired
     private final CloudVendorService cloudVendorService;
 
     @PostMapping("/create")
-    public CreateCloudVendorResponse createCloudVendor(CreateCloudVendorRequest createCloudVendorRequest){
-        return cloudVendorService.createCloudVendor(createCloudVendorRequest);
+    public ResponseEntity<CreateCloudVendorResponse> createCloudVendor(@Valid @RequestBody CreateCloudVendorRequest createCloudVendorRequest){
+        return ResponseEntity.ok(cloudVendorService.createCloudVendor(createCloudVendorRequest));
+    }
+
+    @GetMapping("/{vendorId}")
+    public ResponseEntity<GetCloudVendorResponse> getCloudVendorById(@PathVariable Long vendorId){
+        return ResponseEntity.ok(cloudVendorService.getCloudVendorById(vendorId));
 
     }
 
-    @GetMapping("/vendor/get")
-    public CloudVendor getCloudVendorById(Long vendorId){
-        return cloudVendorService.getCloudVendorById(vendorId);
-    }
-
-    @GetMapping("/vendor/get-all")
+    @GetMapping("/all")
     public List<CloudVendor> getAllCloudVendor(){
         return cloudVendorService.getAllCloudVendor();
     }
 
-    @PutMapping("/update/{vendorId}")
-    public UpdateCloudVendorResponse updateCloudVendor(@PathVariable Long vendorId, UpdateCloudVendorRequest updateCloudVendorRequest){
+    @PutMapping("/{vendorId}")
+    public UpdateCloudVendorResponse updateCloudVendor(@PathVariable Long vendorId, @Valid @RequestBody UpdateCloudVendorRequest updateCloudVendorRequest){
         return cloudVendorService.updateCloudVendor(vendorId, updateCloudVendorRequest);
     }
 
-    @DeleteMapping("/delete/{vendorId}")
+    @DeleteMapping("/{vendorId}")
     public String deleteCloudVendorById(@PathVariable Long vendorId){
-        cloudVendorService.deleteCloudVendor(vendorId);
-        return "cloud vendor deleted successfully";
+        return cloudVendorService.deleteCloudVendorById(vendorId);
     }
 
-    @DeleteMapping("/delete-all")
+    @DeleteMapping("/all")
     public String deleteAllCloudVendors(){
-        cloudVendorService.deleteAllCloudVendors();
-        return "cloud vendor deleted successfully";
+        return cloudVendorService.deleteAllCloudVendors();
     }
-
-
 
 }
