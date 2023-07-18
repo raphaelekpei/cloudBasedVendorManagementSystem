81cc4c8e59977d931d1bbf53ca816439345f407f
Raphael Ekpei <ekpeiraphael020@gmail.com>
2023-07-18 21:10:02 +0100

    Commit 100011

--- src/main/java/com/raphael/cloudbasedvendormanagementsystem/service/CloudVendorService.java
+++ src/main/java/com/raphael/cloudbasedvendormanagementsystem/service/CloudVendorService.java
@@ -1,23 +1,25 @@
 package com.raphael.cloudbasedvendormanagementsystem.service;
 
 import com.raphael.cloudbasedvendormanagementsystem.data.model.CloudVendor;
-import com.raphael.cloudbasedvendormanagementsystem.dtos.request.CreateCloudVendorRequest;
-import com.raphael.cloudbasedvendormanagementsystem.dtos.request.UpdateCloudVendorRequest;
-import com.raphael.cloudbasedvendormanagementsystem.dtos.response.CreateCloudVendorResponse;
-import com.raphael.cloudbasedvendormanagementsystem.dtos.response.UpdateCloudVendorResponse;
+import com.raphael.cloudbasedvendormanagementsystem.dto.request.CreateCloudVendorRequest;
+import com.raphael.cloudbasedvendormanagementsystem.dto.request.UpdateCloudVendorRequest;
+import com.raphael.cloudbasedvendormanagementsystem.dto.response.CreateCloudVendorResponse;
+import com.raphael.cloudbasedvendormanagementsystem.dto.response.GetCloudVendorResponse;
+import com.raphael.cloudbasedvendormanagementsystem.dto.response.UpdateCloudVendorResponse;
 
 import java.util.List;
 
 public interface CloudVendorService {
     CreateCloudVendorResponse createCloudVendor(CreateCloudVendorRequest createCloudVendorRequest);
 
-    CloudVendor getCloudVendorById(Long vendorId);
+    GetCloudVendorResponse getCloudVendorById(Long vendorId);
 
     List<CloudVendor> getAllCloudVendor();
 
     UpdateCloudVendorResponse updateCloudVendor(Long vendorId, UpdateCloudVendorRequest updateCloudVendorRequest);
 
-    void deleteCloudVendor(Long vendorId);
+    String deleteCloudVendorById(Long vendorId);
 
-    void deleteAllCloudVendors();
+    String deleteAllCloudVendors();
+
 }
